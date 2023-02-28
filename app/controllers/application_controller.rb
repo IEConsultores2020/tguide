3f6274834f7177c252fd64d12072954822b942d4
IEConsultores2020 <gerencia@ieconsultores.com>
2023-02-28 12:54:19 -0500

    New translations to ES

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@
 class ApplicationController < ActionController::Base
   # perform the check before each controller action are executed
   before_action :check_concurrent_session
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   private
 
